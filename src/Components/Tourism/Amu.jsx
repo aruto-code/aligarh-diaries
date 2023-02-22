8e66b13e3d88ee555f8b62b3ff32ce58beae37bd
aruto-code <arushee2019@gmail.com>
2023-02-23 02:05:19 +0530

    tourism updates

--- src/Components/Tourism/Amu.jsx
+++ src/Components/Tourism/Amu.jsx
@@ -1,10 +1,52 @@
 import React from "react";
 import Navbar from "../Navbar";
+import TouristCard from "./TouristCard";
+import "../../App.css";
+import Stra from "../../assets/Strachey.jpg";
+import Kennedy from "../../assets/Kennedy.jpg";
+import Jama from "../../assets/Jama.jpg";
+import Musa from "../../assets/Musa.jpg";
+import Syed from "../../assets/Park.jpg";
+import Victoria from "../../assets/VictoriaGate.jpg";
 
 function Amu() {
   return (
     <>
       <Navbar />
+      <div className="about cards">
+        <TouristCard
+          head="Strachey"
+          img={Stra}
+          btnContent="Visit"
+        />
+        <TouristCard
+          head="Kennedy Hall"
+          img={Kennedy}
+          btnContent="Visit"
+        />
+        <TouristCard
+          head="Jama Masjid"
+          img={Jama}
+          btnContent="Visit"
+        />
+      </div>
+      <div className="about cards">
+        <TouristCard
+          head="Musa Dakri Museum"
+          img={Musa}
+          btnContent="Visit"
+        />
+        <TouristCard
+          head="Gulistan e Syed"
+          img={Syed}
+          btnContent="Visit"
+        />
+        <TouristCard
+          head="Victoria Gate"
+          img={Victoria}
+          btnContent="Visit"
+        />
+      </div>
     </>
   );
 }
--- src/Components/Tourism/TouristSpots.jsx
+++ src/Components/Tourism/TouristSpots.jsx
@@ -3,6 +3,11 @@ import Navbar from "../Navbar";
 import "../../App.css";
 import TouristCard from "./TouristCard";
 import khereshwar from "../../assets/khereshwar-temple.jpg";
+import Exhibition from "../../assets/AligarhExhibition.jpg";
+import Iskcon from "../../assets/IskconTemple.jpeg";
+import Sekha from "../../assets/ShekhaJhel.jpg";
+import SirSyed from "../../assets/SirSyedAcademyMuseum.jpg";
+import Teerth from "../../assets/terthdhamMangalayatan.jpg";
 
 function TouristSpots() {
   return (
@@ -15,30 +20,30 @@ function TouristSpots() {
           btnContent="Visit"
         />
         <TouristCard
-          head="Khereshwar Temple"
-          img={khereshwar}
+          head="Aligarh Exhibition"
+          img={Exhibition}
           btnContent="Visit"
         />
         <TouristCard
-          head="Khereshwar Temple"
-          img={khereshwar}
+          head="Iskcon Temple"
+          img={Iskcon}
           btnContent="Visit"
         />
       </div>
       <div className="about cards">
         <TouristCard
-          head="Khereshwar Temple"
-          img={khereshwar}
+          head="Sekha Bird Sanctuary"
+          img={Sekha}
           btnContent="Visit"
         />
         <TouristCard
-          head="Khereshwar Temple"
-          img={khereshwar}
+          head="Sir Syed Academy Museum"
+          img={SirSyed}
           btnContent="Visit"
         />
         <TouristCard
-          head="Khereshwar Temple"
-          img={khereshwar}
+          head="Teerthdham Mangalayatan"
+          img={Teerth}
           btnContent="Visit"
         />
       </div>
