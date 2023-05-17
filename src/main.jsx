8a355cfd064c4995beec0740454c2f28b957c9c6
Sumir Vats <sumirvats@gmail.com>
2023-05-17 15:50:51 +0530

    Added Deployment env

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { Auth0Provider } from "@auth0/auth0-react";
-import config from "../config.json";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Auth0Provider
-    domain={config.domain}
-    clientId={config.clientid}
+    domain={import.meta.env.REACT_APP_DOMAIN}
+    clientId={import.meta.env.REACT_APP_CLIENTID}
     authorizationParams={{
       redirect_uri: window.location.origin,
     }}
